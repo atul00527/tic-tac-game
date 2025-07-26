b1657a4d8e8052e4eef46de03a80b159d2b2d290
Your Name <atul00527>
2025-07-26 22:15:18 +0530

    checkTie, restartGame emplement

--- script.js
+++ script.js
@@ -2,37 +2,57 @@ const gameCells = document.querySelectorAll('.cell');
 const player1 = document.querySelectorAll('.player1');
 const player2 = document.querySelectorAll('.player2');
 const restartBtn = document.querySelectorAll('.restartBtn');
+restartBtn.addEventListener('click', restartGame);
 
-let currentPlayer = 'X'
-let nextPlayer = 'O'
-let playerTurn = currentPlayer
+let currentPlayer = 'X';
+let nextPlayer = 'O';
+let playerTurn = currentPlayer;
+
+player1.textContent = `player 1 : ${currentPlayer}`;
+player2.textContent = `player 2 : ${nextPlayer}`;
 
 const startGame = () => {
     gameCells.forEach(cell => {
-        cell.addEventListener('click', (e) => {
-            if(e.target.textContent = '' ) {
-               e.target.textContent = playerTurn;
-               if ( checkWin() ) {
-                console.log(`${playerTurn} is winning!`);
-               }
-                changePlayerTurn();
-            }
-        });
+        cell.addEventListener('click', handleClick);
     });
-};
-    const changeCurentPlayer = () => {
-        // if(playerTurn === currentPlayer) {
-        //     playerTurn = nextPlayer
+} 
+
+const handleClick = (e) => {
+    if(e.target.textContent === '' ) {
+       e.target.textContent = playerTurn;
+           if ( checkWin()) {
+                console.log(`${playerTurn} is a winning!`);
+                disableCells();
+            }    
+            else if(checkTie()){
+                console.log('it is a Tie')
+                                disableCells();
+
+            }else {
+                changePlayerTurn();
+
+                }
+            }
+    
+}
+// function to change  player turn
+
+    const changePlayerTurn = () => {
+        // if(playerTurn === currentPlayer) {   // cells  write way
+        //     playerTurn = nextPlayer;
         // }
         // else {
-        //     playerTurn = currentPlayer
+        //     playerTurn = currentPlayer;
         // }
-        playerTurn = playerTurn === currentPlayer ?  nextPlayer  :  currentPlayer;
 
-        // function to chek win
+        // turrnery voprartor  wrte way
+
+            playerTurn = playerTurn === currentPlayer ?  nextPlayer  :  currentPlayer;
+
 };
+        // function to check win
         const  checkWin = () => {
-            const winningCondition = [
+            const winningConditions = [
                 [0, 1, 2],
                 [3, 4, 5],
                 [6, 7, 8],
@@ -40,20 +60,46 @@ const startGame = () => {
                 [1, 4, 7],
                 [2, 5, 8],
                 [0, 4, 8],
-                [2, 4, 6],
+                [2, 4, 6]
             ];   
-        for (let i = 0; i <winningCondition.length; i++) {
-            const [pos1,pos2,pos3] = winningCondition[i];
+            
+        for (let i = 0; i < winningConditions.length; i++) {
+            const [pos1,pos2,pos3] = winningConditions[i];
 
             if( gameCells[pos1].textContent !== '' && 
                 gameCells[pos1].textContent === gameCells[pos2].textContent &&
                 gameCells[pos2].textContent === gameCells[pos3].textContent ) {
                 return true;
-                }
-            // console.log(`${pos1},${pos2},${pos3}`);
-            
+            }            
         }
+
         return false;
+    }
+
+        // function check to Tie-----------------------------
+    const checkTie = () => {
+        let emptyCellsCount = 0;
+        gameCells.forEach(cell => {
+            if (cell.textContent === '') {
+                emptyCellsCount++;
+            }
+        });
+
+        return  emptyCellsCount === 0 && !checkWin();
+    }
+    // disable game board cells after a win or tie--------------
+    const disableCells = () => {
+        gameCells.forEach(cell => {
+            cell.removeEventListener('click', handleClick);
+            cell.classList.add('disable')
+        })
+    }
+    const restartGame = () => {
+        gameCells.forEach(cell => {
+            cell.textContent = '';
+            cell.classList.remove('disabled');
+        });
+        restartGame(); 
     }
     startGame();
 
